dfb1ab7b558804f53df16235da6fe94bd6631bcd
Raj Kumar <imraaj1026@gmail.com>
2026-01-12 19:22:02 +0530

    start basic of cpp

---
+++ demp.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <bits/stdc++.h>
+// #include <string>
+// #include <math.h>
+
+
+// 0 demo code
+// using namespace std;
+
+// int main() {
+//     int x;
+//     cin >> x;
+//     cout << "hey " << x;
+// }
+
+// 1. BASICS
+    // use std:: or use using namespace std;
+
+    using namespace std;
+
+int main(){
+
+//    std::cout << "hello Raj!" <<"\n"; //or
+//    std::cout << "hello Raj!" << std::endl ;
+//    std::cout << "hello world" <<std::endl <<"hey pinky";
+
+//   2. INPUT
+    int x, y;
+    cin >>x >> y;
+    cout <<"value of x:" << x << " and y:" << y;
+   return 0;
+}
