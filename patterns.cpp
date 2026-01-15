771de41bd3cd5ea5f4235673d18e6300e320ced3
Raj Kumar <imraaj1026@gmail.com>
2026-01-15 12:17:27 +0530

    done some pattern printings

---
+++ patterns.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+// square pattern
+void patt1(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout << "* " ;
+        }
+        cout << '\n';
+    }
+}
+//  trinangle patter
+void patt2(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout << "* " ;
+        }
+        cout << '\n';
+    }
+}
+//  trinangle (number) patter1
+void patt3(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout << j+1  ;
+        }
+        cout << endl;
+    }
+}
+//  trinangle (number) patter2
+void patt4(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout << i ;
+        }
+        cout << endl;
+    }
+}
+//  inverted trinangle pattern
+void patt5(int n){
+    for(int i=0;i<n;i++){
+        for(int j=n;j>i;j--){
+            cout << "*" ;
+        }
+        cout << endl;
+    }
+}
+//  inverted trinangle pattern(number)
+void patt6(int n){
+    for(int i=0;i<n;i++){
+        for(int j=n;j>i;j--){
+            cout << i ;
+        }
+        cout << endl;
+    }
+}
+
+
+int main(){
+    // square pattern
+    int n;
+    cin >> n;
+    patt4(n);
+}
