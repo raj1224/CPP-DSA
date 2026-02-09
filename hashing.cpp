c647cf8650299112da2654a5e40f65065717e5dd
Raj Kumar <imraaj1026@gmail.com>
2026-02-09 12:13:39 +0530

    basics of hashing started

---
+++ hashing.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// how many time numbers comes in an array
+// T.C and S.C=> O(Q*N)
+// int f(num,arr){
+//     cnt=0;
+//     for(i=0;i<n;i++){
+//         if(arr[i]==num){
+//             cnt=cnt+1;
+//         }
+//     }
+//     return cnt;
+// }
+
+// Hashing:- prestoring / fetching.
+
+
+main(){
+    return 0;
+}
